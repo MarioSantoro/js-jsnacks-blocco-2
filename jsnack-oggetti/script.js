0ccd062a61379e6375168bc77bcec761d33e5bdf
MarioSantoro <mariosantoro124@gmail.com>
2023-05-29 11:47:27 +0200

    add random id

--- jsnack-oggetti/script.js
+++ jsnack-oggetti/script.js
@@ -1,5 +1,5 @@
 let playerBasket = {
-    codiceGiocatore : "ABC123",
+    codiceGiocatore : createIDString(3) + randomNumber(0 , 9) + randomNumber(0 , 9) + randomNumber(0 , 9),
     Nome : "Mimmo",
     Cognome : "Mommmo",
     Età : 23,
@@ -13,4 +13,16 @@ console.log(playerBasket);
 function randomNumber(min , max){
     number = Math.floor(Math.random() * max) - min;
     return number;
+}
+
+function createIDString(length){
+        let result = '';
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        const charactersLength = characters.length;
+        let counter = 0;
+        while (counter < length) {
+          result += characters.charAt(Math.floor(Math.random() * charactersLength));
+          counter += 1;
+        }
+        return result;
 }
